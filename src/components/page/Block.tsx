bbcf9f07f4330e2adb43baa71314d4237a337db7
huseynt <huseyn.tapdiqli.i@gmail.com>
2024-05-17 21:41:19 +0400

    _

--- src/components/page/Block.tsx
+++ src/components/page/Block.tsx
@@ -1,17 +1,16 @@
 import style from './Block.module.scss'
 import { useState } from 'react'
-import {data} from '/src/db/data.tsx'
+import {data} from '../db/data'
 
 function Block() {
   const [first, setFirst] = useState("Low")
-  function btnChange(event) {
+  function btnChange(event: any) {
     setFirst(event.target.innerText)
   }
-
   return (
     <div className={style.app}>
       <div className={style.left}>
-        {data.map(data => { 
+        {data.map((data: any) => { 
           return <div key={data.id} onClick={btnChange} className={style.element}>{data.value}</div>
         })}
       </div>
